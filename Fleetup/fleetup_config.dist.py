0097826e286bd99882d2daa4e888f7004c9b1a30
Kalon Shannon-Innes <mav@hotmail.com.au>
2018-07-02 11:36:52 +0800

    Implement dist fleetup_config.py, add prod fleetup_config to .gitignore

---
+++ Fleetup/fleetup_config.dist.py
@@ -0,0 +1,13 @@
+# Fleetup API Keys
+###################
+# App Key, generated at http://fleet-up.com/Api/MyApps
+# Minimum required permissions are Fittings Read and Doctrines Read
+FU_APP_KEY = "vaxhKjUZLelzPS9urXi2awT9S"
+# API Code, generated at http://fleet-up.com/Api/MyKeys
+FU_API_CODE = "h9rcgiyPKXAcp0ARNHXfbgnMYHd6uI"
+# User id, shown in api code listing
+FU_USER_ID = "9974"
+# Group id, from group view url or via API from /MyGroupMemberships
+FU_GROUP_ID = "48862"
+FU_BASE_URL = "http://api.fleet-up.com/Api.svc"
+FU_APP_URI = "{}/{}/{}/{}".format(FU_BASE_URL, FU_APP_KEY, FU_USER_ID, FU_API_CODE)
