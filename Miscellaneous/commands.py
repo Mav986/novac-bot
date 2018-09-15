3a547b02a9506cfeef9ee9d312f1529b0ebe64ea
Kalon Shannon-Innes <mav@hotmail.com.au>
2018-09-15 20:30:02 +0800

    Add 8ball feature, remove typing indicator for simple commands

--- Miscellaneous/commands.py
+++ Miscellaneous/commands.py
@@ -1,6 +1,7 @@
 from Core._config import BLACKLIST
-from Miscellaneous.config import XKCD_USAGE, NICE_USAGE
+from Miscellaneous.config import *
 from Miscellaneous.controller import get_xkcd_url
+import random
 
 
 class MiscBot:
@@ -8,7 +9,6 @@ class MiscBot:
 
         @slackbot.command('xkcd', help='Display an XKCD webcomic. {}'.format(XKCD_USAGE))
         def xkcd(channel, arg):
-            slackbot.set_typing(channel)
             if arg:
                 args = arg.split(' ', -1)
                 if len(args) == 1 and not any(element.startswith(BLACKLIST) for element in args):
@@ -24,6 +24,14 @@ class MiscBot:
 
         @slackbot.command('nice', help='Receive praise from the Dooster! {}'.format(NICE_USAGE))
         def nice(channel, arg):
-            slackbot.set_typing(channel)
             slackbot.set_personality('U6VJLPC1G')
             return slackbot.post_message(channel, 'Nice', as_user=False)
+
+        @slackbot.command('8ball', help='Need an answer to a yes or no question quickly? {}'.format(EIGHTBALL_USAGE))
+        def eightball(channel, arg):
+            if arg.endswith('?'):
+                message = random.choice(EIGHTBALL_VALID_QUESTION)
+            else:
+                message = random.choice(EIGHTBALL_INVALID_QUESTION)
+
+            return slackbot.post_message(channel, message)
