fbb129decae05d4ddb280386ec3363d1f12625f4
Kalon Shannon-Innes <mav@hotmail.com.au>
2018-10-03 17:18:01 +0800

    Refactor http request to be more secure, update config files

--- Corp/_config.dist.py
+++ Corp/_config.dist.py
@@ -1,26 +1,21 @@
 """
-To get a proper SRP_URL, visit your google form's edit page, click the triple-dot menu in the top right,
-click 'get pre-filled link'. Once on this page, fill out ALL sections with some easily recognizable
-data. I recommend using capital letters to make the next part easier.
+Note: Form ID's are different when viewing/editing than when submitting.
 
-Once the required sections are complete, click 'get link' at the bottom of the page. The page will reload, and
-a new grey popup will be displayed in the bottom left of the window. Click 'copy link' to get the URL. The
-final step is to replace the word 'viewform' with the word 'formResponse'. Remove the placeholder responses you filled
-in earlier, and replace them with the correct string formatting. An example is shown below.
+To get your post form id, visit your google form's edit page, click the triple-dot menu in the top right, and click
+'get pre-filled link'. Fill out all sections using easily recognized placeholder names, then click 'get link' at the
+bottom. The page will reload and a popup will display in the bottom left of the window. Click 'copy link' to get your
+full URL. The form ID will be between the '/e/' and '/viewform' sections.
 
-ENTIRE URL: https://docs.google.com/forms/d/e/1FAIpQLSf86gD81OOSOUVzMqKvD9PQOUER-mAd6pQgeVPDcYjC5Xl-Gw/formResponse
-    ?usp=pp_url&entry.445819805={name}&entry.816115494={killmail_url}&entry.1620172123={extra_info}
-
-Any optional fields of the form can defined as constants and removed from the base URL, later added as optional
-in the implementation. The example is clarified below.
-
-BASE URL: https://docs.google.com/forms/d/e/1FAIpQLSf86gD81OOSOUVzMqKvD9PQOUER-mAd6pQgeVPDcYjC5Xl-Gw/formResponse
-    ?usp=pp_url&entry.445819805={name}&entry.816115494={killmail_url}
-
-OPTIONAL SECTION: &entry.1620172123={extra_info}
-
-Note: Do not split URL onto multiple lines. For some reason, the \ separator breaks the URL to prevent form submission.
+After you've got your form id, you want to get each section that needs filling out. In this new URL, there should
+be several sections following a format of '&entry.#########=YOUR-PLACEHOLDER-NAME'. Each one of those is a section
+that can be filled out. Use these to create the parameter dictionary.
 """
 
-SRP_URL = 'SRP FORMRESPONSE URL HERE'
-SRP_URL_WITH_COMMENT = SRP_URL + 'EXTRA SECTION HERE'
+FORM_ID = 'YOUR-FORM-ID'
+
+ENTRIES = {
+    'name': 'entry.##########',
+    'killmail_url': 'entry.##########',
+    'extry_info': 'entry.##########'
+}
+
--- Corp/controller.py
+++ Corp/controller.py
@@ -1,15 +1,19 @@
 import requests
-from Corp._config import SRP_URL, SRP_URL_WITH_COMMENT
-from Corp.config import LOSS_FORMAT
+from Corp._config import ENTRIES
+from Corp.config import BASE_URL, LOSS_FORMAT
 from Core.esi import get_id
 
 
 def submit_srp(name, url, extra=None):
+    params = {
+        'usp': 'pp_url',
+        ENTRIES['name']: name,
+        ENTRIES['killmail_url']: url}
     if extra:
         extra = extra.strip()
-        requests.post(SRP_URL_WITH_COMMENT.format(name=name, killmail_url=url, extra_info=extra))
-    else:
-        requests.post(SRP_URL.format(name=name, killmail_url=url))
+        params[ENTRIES['extra_info']] = extra
+
+    requests.post(BASE_URL, params=params)
 
 
 def valid_lossmail(url):
