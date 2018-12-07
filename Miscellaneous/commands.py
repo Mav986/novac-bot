6bb444dd40e8565c101166d8ad77280c7bd108f1
Kalon Shannon-Innes <mav@hotmail.com.au>
2018-12-08 05:17:09 +0800

    Fix wh command to pull attributes from esi directly

--- Miscellaneous/commands.py
+++ Miscellaneous/commands.py
@@ -19,7 +19,6 @@ class MiscBot:
 
             return slackbot.post_message(channel, message)
 
-
         @slackbot.command('dooster', help='Ask the Dooster a question! {}'.format(NICE_USAGE))
         def dooster(channel, arg, user):
             slackbot.mimic_user('U6VJLPC1G')
@@ -27,7 +26,6 @@ class MiscBot:
 
             return slackbot.post_message(channel, message, as_user=False)
 
-         
         @slackbot.command('8ball', help='Need an answer to a yes or no question quickly? {}'.format(EIGHTBALL_USAGE))
         def eightball(channel, arg, user):
             if arg.endswith('?'):
@@ -45,11 +43,20 @@ class MiscBot:
                     message = K162ERROR
                 else:
                     wh_info = get_wormhole_stats(wormhole_id)
-                    if wh_info:
-                        message = WORMHOLE_ATTR.format(wormhole_id=wormhole_id, leads_to=wh_info["leadsTo"],
-                                                       jump_mass=wh_info["jumpMass"],
-                                                       total_mass=wh_info["totalMass"],
-                                                       lifetime=wh_info["maxLifetime"])
+                    if wh_info and wh_info.get('regen_mass') == 0:
+                        message = WORMHOLE_ATTR.format(
+                            wormhole_id=wormhole_id, leads_to=wh_info.get('leads_to'),
+                            jump_mass=wh_info.get('jump_mass'),
+                            total_mass=wh_info.get('total_mass'),
+                            lifetime=wh_info.get('lifetime'))
+
+                    elif wh_info and wh_info.get('regen_mass') > 0:
+                        message = WORMHOLE_ATTR_REGEN.format(
+                            wormhole_id=wormhole_id, leads_to=wh_info.get('leads_to'),
+                            jump_mass=wh_info.get('jump_mass'),
+                            total_mass=wh_info.get('total_mass'),
+                            regenMass=wh_info.get('regen_mass'),
+                            lifetime=wh_info.get('lifetime'))
                     else:
                         message = "Wormhole type not found"
             else:
--- Miscellaneous/config.py
+++ Miscellaneous/config.py
@@ -51,6 +51,12 @@ WORMHOLE_ATTR = "*{wormhole_id}* leads to _{leads_to}_\n" \
                 "*Total Mass*: {total_mass} kg\n" \
                 "*Lifetime*: {lifetime}h"
 
+WORMHOLE_ATTR_REGEN = "*{wormhole_id}* leads to _{leads_to}_\n" \
+                      "*Size*: {jump_mass}\n" \
+                      "*Total Mass*: {total_mass} kg\n" \
+                      "*Regen*: {regenMass} kg per cycle\n" \
+                      "*Lifetime*: {lifetime}h"
+
 
 JUMP_MASS_CATEGORIES = {
     5000000: "Small",
@@ -58,5 +64,25 @@ JUMP_MASS_CATEGORIES = {
     300000000: "Large",
     1000000000: "Freighter",
     1350000000: "Very Large",
+    1480000000: "Very Large",
     1800000000: "Very Large"
+}
+
+DESTINATION_CATEGORIES = {
+    1: "Class 1 W-Space",
+    2: "Class 2 W-Space",
+    3: "Class 3 W-Space",
+    4: "Class 4 W-Space",
+    5: "Class 5 W-Space",
+    6: "Class 6 W-Space",
+    7: "Highsec",
+    8: "Lowsec",
+    9: "Nullsec",
+    12: "Thera",
+    13: "Class 13 W-Space",
+    14: "the Sentinel Drifter Wormhole",
+    15: "the Barbican Drifter Wormhole",
+    16: "the Vidette Drifter Wormhole",
+    17: "the Conflux Drifter Wormhole",
+    18: "the Redoubt Drifter Wormhole"
 }
