b27e265e6a2c046739fbe662a46d94ef61465900
Kalon Shannon-Innes <mav@hotmail.com.au>
2019-06-13 23:35:20 +0800

    Fix Core config file format

--- Core/_config.dist.py
+++ Core/_config.dist.py
@@ -1,11 +1,13 @@
 # Discord Config
 # Bot token, generated via discord developer portal:
-BOT_TOKEN = 'BOT-TOKEN-HERE'
+TOKEN = 'BOT TOKEN HERE'
 # Delay between reads in seconds
-BOT_READ_DELAY = 1
+READ_DELAY = 1
+# Prefix to use for bot commands
+PREFIX = '!'
 
 # ESI Config
 ESI_BASE_URL = 'https://esi.evetech.net'
 ESI_DATASOURCE = 'tranquility'
 ESI_SWAGGER_JSON = '{}/_latest/swagger.json?datasource={}'.format(ESI_BASE_URL, ESI_DATASOURCE)
-ESI_USER_AGENT = 'USER-AGENT-HERE'
+ESI_USER_AGENT = 'USER AGENT HERE'
